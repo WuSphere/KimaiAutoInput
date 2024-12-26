aa5291c55d5962050462868fac47fa85dc94e74c
楊 慶武 <wsstj_you_ke@ines.co.jp>
2024-12-26 17:52:33 +0900

    fix

---
+++ UpdateTimestamps.ps1
@@ -0,0 +1,11 @@
+﻿$folderPath = "C:\DriveH\GitHub\KimaiAutoInput"
+$newTime = Get-Date  # Get current system time
+
+# Recursively update all files and folders
+Get-ChildItem -Path $folderPath -Recurse | ForEach-Object {
+    $_.CreationTime = $newTime
+    $_.LastWriteTime = $newTime
+    $_.LastAccessTime = $newTime
+}
+
+Write-Output "Timestamps updated to the current time for all files and folders in $folderPath"
